4e5abbecceba101a9f219ec1262bde1f384a1dbf
최연영 <>
2025-03-28 01:58:22 +0900

    test: 사용자 포인트 사용 테스트

--- src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -73,4 +73,71 @@ class PointServiceTest {
         verify(userPointRepository, never()).insertOrUpdate(anyLong(), anyLong());
         verify(pointHistoryRepository, never()).insert(anyLong(), anyLong(), any(TransactionType.class), anyLong());
     }
+
+    @Test
+    @DisplayName("사용자_포인트_사용_성공_테스트")
+    void useUserPoint_Success_Test() {
+        // given
+        long userId = 1L;
+        long currentPoint = 100L;
+        long useAmount = 30L;
+        long remainingPoint = (currentPoint - useAmount);
+        long updateMillis = System.currentTimeMillis();
+        // 1. 사용자 조회
+        given(userPointRepository.selectById(userId)).willReturn(new UserPoint(userId, currentPoint, updateMillis));
+        // 2. 포인트 저장
+        given(userPointRepository.insertOrUpdate(userId, remainingPoint))
+                .willReturn(new UserPoint(userId, remainingPoint, updateMillis));
+
+        // when
+        UserPoint updateUserPoint = pointService.useUserPoint(userId, useAmount, updateMillis);
+
+        // then
+        assertThat(updateUserPoint.point()).isEqualTo(remainingPoint);
+        verify(userPointRepository).selectById(eq(userId));
+        verify(userPointRepository).insertOrUpdate(eq(userId), eq(remainingPoint));
+        verify(pointHistoryRepository).insert(eq(userId), eq(useAmount), eq(TransactionType.USE), eq(updateMillis));
+    }
+
+    @Test
+    @DisplayName("사용자_포인트_사용_잔액_부족_실패_테스트")
+    void useUserPoint_insufficient_Fail_Test() {
+        // given
+        long userId = 1L;
+        long currentPoint = 100L;
+        long useAmount = 150L;
+        long updateMillis = System.currentTimeMillis();
+        // 1. 사용자 조회
+        given(userPointRepository.selectById(userId)).willReturn(new UserPoint(userId, currentPoint, updateMillis));
+
+        // when & then
+        assertThatThrownBy(() -> pointService.useUserPoint(userId, useAmount, updateMillis))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MessageConstants.INSUFFICIENT_POINTS);
+
+        verify(userPointRepository, never()).insertOrUpdate(anyLong(), anyLong());
+        verify(pointHistoryRepository, never()).insert(anyLong(), anyLong(), any(TransactionType.class), anyLong());
+    }
+
+    @Test
+    @DisplayName("사용자_포인트_요청_음수_예외처리_테스트")
+    void throwException_validateAmount_Test() {
+        // given
+        long userId = 1L;
+        long amount = -500L;
+        long updateMillis = System.currentTimeMillis();
+        // 1. 사용자 조회
+        given(userPointRepository.selectById(userId)).willReturn(new UserPoint(userId, 100L, updateMillis));
+
+        // when & then
+        // 2. 충전 포인트 음수 예외처리
+        assertThatThrownBy(() -> pointService.chargeUserPoint(userId, amount, updateMillis))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MessageConstants.INVALID_AMOUNT);
+
+        // 3. 사용 포인트 음수 예외처리
+        assertThatThrownBy(() -> pointService.useUserPoint(userId, amount, updateMillis))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MessageConstants.INVALID_AMOUNT);
+    }
 }
